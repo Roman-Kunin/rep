fc2cd38a82923bd456fea4cbca74ee0215810c87
Roman-Kunin <romantop1414@gmail.com>
2024-12-02 11:56:27 +0300

    A significant upgrade

--- Study/Prime_list.py
+++ Study/Prime_list.py
@@ -1,15 +1,27 @@
-number = 2
-if_prime = True
-primes_amount = 0
+# number = 2
+# if_prime = True
+# primes_amount = 0
 
-while primes_amount != 500:
-    if_prime = True
-    for n in range(2, number):
-        if number % n == 0:
-            if_prime = False
+# while primes_amount != 500:
+#     if_prime = True
+#     for n in range(2, number):
+#         if number % n == 0:
+#             if_prime = False
+#             break
+
+#     if if_prime is True:
+#         primes_amount += 1
+#         print(f"{primes_amount}.   {number}")
+#     number += 1
+
+
+prime_list = []
+i = 2
+while len(prime_list) != 500:
+    for j in range(2, int(i**0.5)+1):
+        if not (i % j):
             break
-
-    if if_prime is True:
-        primes_amount += 1
-        print(f"{primes_amount}.   {number}")
-    number += 1
+    else:
+        prime_list.append(i)
+    i+=1
+[print(f'{ind}. {i}') for ind, i in enumerate(prime_list, start=1)]
